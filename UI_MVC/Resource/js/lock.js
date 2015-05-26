d43aff72065bed0fb48750752496e4e21db53be2
yang_xun <yang_xun@outlook.com>
2015-05-26 12:03:53 +0800

    添加锁屏页面

--- UI_MVC/Resource/js/lock.js
+++ UI_MVC/Resource/js/lock.js
@@ -1,14 +1,14 @@
 var Lock = function () {
-
+    
     return {
         //main function to initiate the module
         init: function () {
 
              $.backstretch([
-		        "assets/img/bg/1.jpg",
-		        "assets/img/bg/2.jpg",
-		        "assets/img/bg/3.jpg",
-		        "assets/img/bg/4.jpg"
+		        "/Resource/image/bg/1.jpg",
+		        "/Resource/image/bg/2.jpg",
+		        "/Resource/image/bg/3.jpg",
+		        "/Resource/image/bg/4.jpg"
 		        ], {
 		          fade: 1000,
 		          duration: 8000
