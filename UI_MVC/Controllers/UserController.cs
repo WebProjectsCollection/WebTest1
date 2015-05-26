bfa25dbe1f4a8faefa929d3bea06b1cfe91c452a
yang_xun_home <yang_xun@outlook.com>
2015-05-26 20:45:46 +0800

    又搞丢一个

---
+++ UI_MVC/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string password)
+        {
+            // username=yx&password=123
+            if (userName == "yx" && password == "123")
+            {
+                return Json(new { Message = "OK" });
+            }
+            else
+            {
+                return Json(new { Message = "Error" });
+            }
+        }
+
+    }
+}
